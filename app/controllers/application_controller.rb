83f82083d6636e7aaa9b116e96ca8045769870cd
Natalisp <nathalipat@gmail.com>
2016-09-01 12:36:02 -0400

    updated models and index page style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views/'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+
+  get '/' do 
+    binding.pry
+    erb :index
+  end
+
+  get '/signup' do 
+    erb :'/users/create_user'
+  end
+
+
 end
