752d786765e80845ce2f38314903d8b5442462a1
Natalisp <nathalipat@gmail.com>
2016-09-01 14:55:17 -0400

    first app controller draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,34 @@ class ApplicationController < Sinatra::Base
 
 
   get '/' do 
-    binding.pry
-    erb :index
+     if user_signed_in?
+      erb :'users/profile'
+    else
+      erb :'users/auth'
+    end
   end
 
-  get '/signup' do 
-    erb :'/users/create_user'
+   helpers do
+    def current_user
+      @_current_user ||= User.find_by(id: session[:id])
+    end
+
+    def user_signed_in?
+      !!current_user
+    end
+
+    def login(user)
+      session[:id] = user.id
+    end
+
+    def logout
+      session[:id] = nil
+    end
   end
 
+  not_found do
+    status 404
+    erb :oops
+  end
 
 end
--- db/migrate/20160901153424_create_books.rb
+++ db/migrate/20160901153424_create_books.rb
@@ -1,4 +1,8 @@
 class CreateBooks < ActiveRecord::Migration
   def change
+    create_table :books do |t|
+      t.string :name
+      t.integer :user_id
+    end
   end
 end
